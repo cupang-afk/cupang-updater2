b1dd90badf123a91ccb402a2fbe1e68dd2bf7ce7
cupang-afk <pixelview56@gmail.com>
2025-02-25 10:34:16 +0700

    docs: config comments

--- src/cupang_updater/config/default.py
+++ src/cupang_updater/config/default.py
@@ -23,18 +23,19 @@ last_update:
 
 settings:
   # server_folder can be determinted by one of the following types:
-  # - Local: Use a local file path, e.g., /home/user/minecraft_server
-  # - SFTP: Use an SFTP URL in the format sftp://username:password@example.com:port/home/user/minecraft_server
-  #         where 'username' is your SFTP username, 'password' is your SFTP password, 'example.com' is the host,
-  #         and 'port' is the connection port (default to 22).
-  # - FTP: Use an FTP URL in the format ftp://username:password@example.com:port/home/user/minecraft_server
-  #        where 'username' is your FTP username, 'password' is your FTP password, 'example.com' is the host,
-  #        and 'port' is the connection port (default to 21).
-  # - SMB: Use an SMB URL in the format smb://username:password@example.com:port/share/minecraft_server
-  #        where 'username' is your SMB username (for Microsoft account use "MicrosoftAccount\\user%40email.com"),
-  #        'password' is your SMB password, 'example.com' is the host, and 'port' is the connection port (default to 445).
-  #        Note: %40 in the username will be converted to @.
-  #              (e.g., "MicrosoftAccount\\user%40email.com" will be converted to "MicrosoftAccount\\user@email.com")
+  #
+  # - Local:  Use a local file path, e.g., /home/user/minecraft_server
+  # - SFTP:   Use an SFTP URL in the format sftp://username:password@example.com:port/home/user/minecraft_server
+  #           where 'username' is your SFTP username, 'password' is your SFTP password, 'example.com' is the host,
+  #           and 'port' is the connection port (default to 22).
+  # - FTP:    Use an FTP URL in the format ftp://username:password@example.com:port/home/user/minecraft_server
+  #           where 'username' is your FTP username, 'password' is your FTP password, 'example.com' is the host,
+  #           and 'port' is the connection port (default to 21).
+  # - SMB:    Use an SMB URL in the format smb://username:password@example.com:port/share/minecraft_server
+  #           where 'username' is your SMB username (for Microsoft account use "MicrosoftAccount\\user%40email.com"),
+  #           'password' is your SMB password, 'example.com' is the host, and 'port' is the connection port (default to 445).
+  #           Note: %40 in the username will be converted to @.
+  #                 (e.g., "MicrosoftAccount\\user%40email.com" will be converted to "MicrosoftAccount\\user@email.com")
   # - Webdav: Use an Webdav URL in the format webdav://username:password@example.com:port/home/user/minecraft_server
   #           where 'username' is your Webdav username, 'password' is your Webdav password, 'example.com' is the host,
   #           and 'port' is the connection port (default to 80 for webdav and 443 for webdavs).
